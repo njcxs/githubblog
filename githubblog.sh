28c35b21b9d21884fc63edae9d285a8ba4d9a8fe
njcx <1666276469@qq.com>
2018-09-24 07:40:52 +0800

    NM

--- githubblog.sh
+++ githubblog.sh
@@ -1,1 +1,1 @@
-make html && git add . && git commit -m "NM" && git push -u origin master && cd output/ && git add . && git commit -m "NM" && git push -u origin master
+make html && git add . && git commit -m "NM" && git push -u origin master && cd output/ && git add . && git commit -m "NM" && git push -u origin master && cd ..
